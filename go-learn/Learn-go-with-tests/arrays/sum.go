6f0d83f91aaecf0c0f7d131d4506148e1b91b150
zhou <624061566@qq.com>
2020-05-29 00:48:31 +0800

    pointer

--- go-learn/Learn-go-with-tests/arrays/sum.go
+++ go-learn/Learn-go-with-tests/arrays/sum.go
@@ -2,17 +2,31 @@ package arrays
 
 func Sum(array []int) int {
 	sum := 0
-	for _,i:= range(array) {
+	for _, i := range array {
 		sum += i
 	}
 	return sum
 }
 
-func SumAll(numebrsToSum ...[]int) (sums []int){
-	lengthOfNmbers := len(numebrsToSum)
-	sums = make([]int, lengthOfNmbers)
-	for i, numbers := range numebrsToSum {
-		sums[i] = Sum(numbers)
+func SumAll(numebrsToSum ...[]int) []int {
+	//lengthOfNmbers := len(numebrsToSum)
+	//sums = make([]int, lengthOfNmbers)
+	var sums []int
+	for _, numbers := range numebrsToSum {
+		sums = append(sums, Sum(numbers))
 	}
-	return
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
+
 }
